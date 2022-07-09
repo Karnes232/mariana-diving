d6ff05bfe30dd3212bb1b60c1cd0a885411ec494
James Karnes <karnes.james@gmail.com>
2022-07-09 14:32:42 -0400

    undoing full screen

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -6,24 +6,20 @@ import Layout from "../components/layout"
 import dsdCardImage from "../images/dsdCard.jpg"
 import sdCardImage from "../images/sdCard.jpg"
 import owdCardImage from "../images/owdCard.jpg"
-import VideoComponent from "../components/CourseCardComponents/VideoComponent"
 
 const courses = () => {
   return (
     <Layout>
       <div className="w-screen max-w-lg flex flex-col mt-5 items-center">
         <div className="relative w-screen max-w-lg h-56">
-        <VideoComponent
-          url={'https://www.youtube.com/watch?v=KvZT3etZIsw'}
-        />
-          {/* <ReactPlayer
+          <ReactPlayer
             className="absolute top-0 left-0"
             url="https://www.youtube.com/watch?v=KvZT3etZIsw"
             //url='https://www.youtube.com/watch?v=Ad5iorC7xlo'
             //url="https://www.youtube.com/watch?v=qEzf4MjmowA"
             width="100%"
             height="100%"
-          /> */}
+          />
         </div>
         <div className="flex flex-col items-center justify-center mt-5">
           <CourseCard
